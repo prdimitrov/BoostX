08240a3980a285cf40f6eabe456f9b9e75b7173c
prdimitrov <prdimitrov97@gmail.com>
2024-08-31 18:59:23 +0300

    One more thing added for the moment.
    - Added two methods, called ProcessInput and ProcessRotation, that are currently only logging in the console for now.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start() { }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessInput();
+        ProcessRotation();
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Pressed SPACE - Thrusting");
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("rotate left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("rotate right");
+        }
     }
 }
