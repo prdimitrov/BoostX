9231dadf9bc174b1efaf2df3b8b75dfd05f08f96
prdimitrov <prdimitrov97@gmail.com>
2024-09-03 13:59:02 +0300

    Added a reload mechanism.
    - Added a ReloadLevel method
    - The ReloadLevel method uses the SceneManager to load the current active scene, if the player accidentally hits something that he shouldn't!

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,22 +1,29 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    void OnCollisionEnter(Collision other) 
-    { 
-        switch (other.gameObject.tag) 
+    void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
         {
             case "Friendly":
-            Debug.Log("Player touched something friendly!");
-            break;
+                Debug.Log("Player touched something friendly!");
+                break;
             case "Finish":
-            Debug.Log("Player reached the finish line!");
-            break;
+                Debug.Log("Player reached the finish line!");
+                break;
             default:
-            Debug.Log("Player just crashed into something!");
-            break;
+                Debug.Log("Player just crashed into something!");
+                ReloadLevel();
+                break;
         }
+    }
 
+    private void ReloadLevel()
+    {
+        // SceneManager.LoadScene(0);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
